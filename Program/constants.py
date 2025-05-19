4aeb27c3ae2db82b9b431b1ff304437ea5c719b6
JM-13 <visockas13@gmail.^com>
2025-05-19 12:51:24 +0300

    added required order of constants for rewritten better code

--- Program/constants.py
+++ Program/constants.py
@@ -3,6 +3,7 @@
 S0_folder = '/home/deck/Desktop/Bak_Baig/(JV)/bdpolarity/1-S0'
 S1_folder = '/home/deck/Desktop/Bak_Baig/(JV)/bdpolarity/2-S1'
 Scan_folder = '/home/deck/Desktop/Bak_Baig/(JV)/bdpolarity/3-scans'
+Procesed_Data_Folder = '/home/deck/Desktop/Bak_Baig/Program/Data/'
 
 Solvent_to_fname = {'Acetone':'-acet',
                     'DiMethylFormamide':'-dmfm',
@@ -16,3 +17,16 @@ Material_to_fname = {'BDP-nitrophenyl':'bdp-pnphen',
                      'BDP-PP-phenyl':'ppp',
                      'BDP-PP-phenyl_OMes_backward':'ppp-ome-out',
                      'BDP-PP-phenyl_OMes_forward':'ppp-ome-in'}
+
+Filename_to_Solvent = {'acet':'Acetone',
+                       'dmfm':'DiMethylFormamide',
+                       'dmso':'DiMethylSulfoxide',
+                       'meoh':'Methanol',
+                       'thfu':'TetraHydroFuran',
+                       'tolu':'Toluene'}
+
+Filename_to_Solute = {'bdp-pnphen' :'BDP-nitrophenyl',
+                      'bdp-phenyl' :'BDP-phenyl',
+                      'ppp'        :'BDP-PP-phenyl',
+                      'ppp-ome-out':'BDP-PP-phenyl_OMes_backward',
+                      'ppp-ome-in' :'BDP-PP-phenyl_OMes_forward'}
