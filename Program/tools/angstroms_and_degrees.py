382925dfba8c358da63689f521d36d4f4bcc49e4
JM-13 <visockas13@gmail.^com>
2025-05-19 18:15:59 +0300

    made it more flexible

--- Program/tools/angstroms_and_degrees.py
+++ Program/tools/angstroms_and_degrees.py
@@ -1,15 +1,14 @@
+import pandas as pd
+import numpy as np
 
-
-def Convert_measurement_type(row):
-
-    rounding = [True, 6]
+def Convert_measurement_type(row, rounding=True, rounding_digits=6):
 
     if 'R' in row.iloc[0]:
         converted_value = row.iloc[-1] * 0.529177249 #To angstroms
     else:
         converted_value = np.degrees(row.iloc[-1])   #To degrees
 
-    if rounding[0]:
-        converted_value = round(converted_value, rounding[1])
+    if rounding:
+        converted_value = round(converted_value, rounding_digits)
 
     return converted_value
