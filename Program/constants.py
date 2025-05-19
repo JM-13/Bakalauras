d9b8f79135e83b8662ed6a28666f686d78c5ec58
JM-13 <visockas13@gmail.^com>
2025-05-19 18:18:42 +0300

    removed depricated code and its costants

--- Program/constants.py
+++ Program/constants.py
@@ -4,19 +4,6 @@ S0_folder = '/home/deck/Desktop/Bak_Baig/(JV)/bdpolarity/1-S0'
 S1_folder = '/home/deck/Desktop/Bak_Baig/(JV)/bdpolarity/2-S1'
 Scan_folder = '/home/deck/Desktop/Bak_Baig/(JV)/bdpolarity/3-scans'
 
-Solvent_to_fname = {'Acetone':'-acet',
-                    'DiMethylFormamide':'-dmfm',
-                    'DiMethylSulfoxide':'-dmso',
-                    'Methanol':'-meoh',
-                    'TetraHydroFuran':'-thfu',
-                    'Toluene':'-tolu'}
-
-Material_to_fname = {'BDP-nitrophenyl':'bdp-pnphen',
-                     'BDP-phenyl':'bdp-phenyl',
-                     'BDP-PP-phenyl':'ppp',
-                     'BDP-PP-phenyl_OMes_backward':'ppp-ome-out',
-                     'BDP-PP-phenyl_OMes_forward':'ppp-ome-in'}
-
 Filename_to_Solvent = {'acet':'Acetone',
                        'dmfm':'DiMethylFormamide',
                        'dmso':'DiMethylSulfoxide',
