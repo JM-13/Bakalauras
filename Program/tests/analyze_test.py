235b587b1af58ea386649c2f9221d0a92c85daf7
JM-13 <visockas13@gmail.^com>
2025-12-10 19:30:27 +0200

    tested new atom number sorting

--- Program/tests/analyze_test.py
+++ Program/tests/analyze_test.py
@@ -17,12 +17,13 @@ solute_filename  = "Differences_between_solutes.tex"
 function = DifferenceFunction.symmetric_percentage_difference
 
 analasys = Analyze(folder, function, solutes, solvents, solutes_shorten, solvents_shorten)
+analasys.solute_differences(bdp_central="H", mapping=c.Atom_number_conversion)
 
-analasys.solvent_differences()
-# analasys.display_solvent_differences()
-# analasys.display_solvent_differences_by_solute()
-analasys.generate_latex_results_document(differences="Solvent",
-                                         use_solvent_by_solute=False)
+# analasys.solvent_differences()
+# # analasys.display_solvent_differences()
+# # analasys.display_solvent_differences_by_solute()
+# analasys.generate_latex_results_document(differences="Solvent",
+#                                          use_solvent_by_solute=False)
 
 # analasys.solute_differences(bdp_central="A")
 # analasys.display_solute_differences()
