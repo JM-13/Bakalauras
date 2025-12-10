4d08ef8dd9614683ff81aa446373ec41df98582f
JM-13 <visockas13@gmail.^com>
2025-12-10 19:26:47 +0200

    added atom numbering conversion dictionary

--- Program/constants.py
+++ Program/constants.py
@@ -1,12 +1,11 @@
 from tools.calculation_functions import DifferenceFunction
 
 # Folders of files
-S0_folder = '/home/deck/Desktop/Bak_Baig/(JV)/bdpolarity/1-S0'
-S1_folder = '/home/deck/Desktop/Bak_Baig/(JV)/bdpolarity/2-S1'
+S0_folder   = '/home/deck/Desktop/Bak_Baig/(JV)/bdpolarity/1-S0'
+S1_folder   = '/home/deck/Desktop/Bak_Baig/(JV)/bdpolarity/2-S1'
 Scan_folder = '/home/deck/Desktop/Bak_Baig/(JV)/bdpolarity/3-scans'
 
-Save_optimized_coordinates = False
-Use_angstroms_and_degrees = False
+Save_optimized_coordinates = False #they are less accurate and double rounded
 difference_function = DifferenceFunction.symmetric_percentage_difference
 
 
@@ -40,3 +39,29 @@ Solute_to_shorten = {'BDP-nitrophenyl':'BDP-NPh',
                      'BDP-PP-phenyl':'BDP-PPPh',
                      'BDP-PP-phenyl_OMes_backward':'OMe-out',
                      'BDP-PP-phenyl_OMes_forward':'OMe-in'}
+
+#Atoms are numbered differently
+#keys are:   BDP-NPh and BDP-Ph
+#values are: BDP-PPPh, BDP-PPPh-OMe-out and BDP-PPPh-OMe-in
+#4,6,8,13,15,17 are H. 6:41 and 15:30 replace H with C
+Atom_number_conversion = { 1:19,
+                           2:17,
+                           3:16,
+                           4:29,
+                           5:15,
+                           6:41,
+                           7:14,
+                           8:28,
+                           9:13,
+                          10:12,
+                          11:1,
+                          12:2,
+                          13:21,
+                          14:3,
+                          15:30,
+                          16:4,
+                          17:22,
+                          18:5,
+                          19:18,
+                          20:20,
+                          21:11}
